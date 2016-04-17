a60647418b11d3bcc92a41388c0a9091ccd71f9b
Mengxiao Lin <linmx0130@163.com>
2016-04-17 12:42:56 +0800

    add some news

--- src/monopoly/cell/NewsCell.java
+++ src/monopoly/cell/NewsCell.java
@@ -4,6 +4,7 @@ import message.NewsMessage;
 import monopoly.Bank;
 import monopoly.Kernel;
 import monopoly.Player;
+import monopoly.card.CardFactory;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -17,19 +18,21 @@ public class NewsCell extends AbstractCell {
     public static ArrayList<NewsMessage.NewsEffect> effects = new ArrayList<>();
     public static ArrayList<String> effectDescriptions = new ArrayList<>();
     static {
+        //New 1
         effects.add( (message, _e) -> {
+            int maxPropertyCount = Kernel.getInstance().getPlayers().stream().map(
+                    p->p.getPropertyCells().size()
+                ).reduce(0, (a,b) -> Integer.max(a,b));
+            Stream<Player> riches = Kernel.getInstance().getPlayers().stream().filter(
+                    player-> player.getPropertyCells().size() == maxPropertyCount
+            );
             double maxPropertyValue = Kernel.getInstance().getPlayers().stream().map(
                     player -> player.getPropertyCells().stream().map(
                             cell -> cell.getBuyingPrice()
                     ).reduce(0.0, (a,b) -> a+b)
             ).reduce(0.0, (a,b)-> Double.max(a,b));
-            Stream<Player> riches = Kernel.getInstance().getPlayers().stream().filter(
-                    player-> player.getPropertyCells().stream().map(
-                            cell -> cell.getBuyingPrice()
-                    ).reduce(0.0,(a,b)->a+b) == maxPropertyValue
-            );
-            ArrayList<String> richesName =new ArrayList<>();
 
+            ArrayList<String> richesName =new ArrayList<>();
             StringBuffer buffer=new StringBuffer("公开表扬第一地主");
             riches.forEach(player -> {
                 richesName.add(player.getName());
@@ -42,6 +45,8 @@ public class NewsCell extends AbstractCell {
             message.setDescription(buffer.toString());
         });
         effectDescriptions.add("公开表扬第一地主xx，奖励yy元！");
+
+        //News 2
         effects.add( (_message, _player) -> {
             Kernel.getInstance().getPlayers().stream().forEach(player -> {
                 Bank bank=Kernel.getInstance().getBank();
@@ -49,6 +54,45 @@ public class NewsCell extends AbstractCell {
             });
         });
         effectDescriptions.add("所有人缴纳财产税10%：存款扣除10%！");
+
+        //News 3
+        effects.add( (_message, _player)->{
+            double maxPropertyValue = Kernel.getInstance().getPlayers().stream().map(
+                    player -> player.getPropertyCells().stream().map(
+                            cell -> cell.getBuyingPrice()
+                    ).reduce(0.0, (a,b) -> a+b)
+            ).reduce(0.0, (a,b)-> Double.max(a,b));
+            int minPropertyCount = Kernel.getInstance().getPlayers().stream().map(
+                    player-> player.getPropertyCells().size()
+            ).reduce(Integer.MAX_VALUE, (a,b) -> Integer.min(a,b));
+            Stream<Player> poorest = Kernel.getInstance().getPlayers().stream()
+                    .filter(player -> player.getPropertyCells().size() == minPropertyCount);
+            ArrayList<String> poorestNames = new ArrayList<>();
+            StringBuffer buffer = new StringBuffer("公开补助土地最少者");
+            poorest.forEach(player -> {
+                poorestNames.add(player.getName());
+                player.setMoney(player.getMoney()+maxPropertyValue * 0.1);
+                buffer.append(player.getName()+"，");
+            });
+            buffer.append(String.valueOf(maxPropertyValue*0.1));
+            buffer.append("元");
+            _message.setDescription(buffer.toString());
+        });
+        effectDescriptions.add("公开补助土地最少者xx，xxx元");
+
+        //News 4
+        effects.add( (_message, _player) -> Kernel.getInstance().getPlayers().stream().forEach(player -> {
+            Bank bank=Kernel.getInstance().getBank();
+            bank.modifyMoney(player, bank.getDeposit(player)*0.1);
+        }));
+        effectDescriptions.add("银行加发储金红利，每个人得到存款10%！");
+
+        //News 5
+        effects.add( (message, _player) ->
+            Kernel.getInstance().getPlayers().forEach(player ->
+                player.addCard(Kernel.getInstance().getCardFactory().generateRandomCard())
+            ));
+        effectDescriptions.add("每个人得到一张卡片。");
     }
     public NewsCell(int id){
         super(id, "新闻点", "专门负责搞大新闻。");
